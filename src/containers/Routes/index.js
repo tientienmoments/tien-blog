c066e309b79690adc79f69ebb6d0e1f49344d074
tientienmoments <tientienmoments@gmail.com>
2020-09-08 17:31:25 +0700

    tien add not found page

--- src/containers/Routes/index.js
+++ src/containers/Routes/index.js
@@ -3,12 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import DashboardPage from "../DashboardPage";
 import PrivateRoute from "./PrivateRoute";
 import PublicLayout from "../../layouts/PublicLayout";
-
+import NotFoundPage from "../../components/NotFoundPage"
 const Routes = (props) => {
   return (
     <Switch>
       <PrivateRoute exact path="/dashboard" component={DashboardPage} />
       <Route path="/" component={PublicLayout} />
+      <Route component={NotFoundPage} />
     </Switch>
   );
 };
